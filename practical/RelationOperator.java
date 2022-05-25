aef9d69f05a1b5727ee96180888d4fd38edec077
AF0182636 <105783992+AF0182636@users.noreply.github.com>
2022-05-25 11:31:43 +0530

    Add files via upload

---
+++ practical/RelationOperator.java
@@ -0,0 +1,17 @@
+package SACHIN;
+
+public class RelationOperator {
+public static void main(String[] args) {
+		
+		
+		int num1 = 12, num2 = 4;
+		
+		System.out.println("num1 == num2 = " + (num1 == num2) );
+		System.out.println("num1 != num2 = " + (num1 != num2) );
+		System.out.println("num1 >  num2 = " + (num1 >  num2) );
+		System.out.println("num1 <  num2 = " + (num1 <  num2) );
+		System.out.println("num1 >= num2 = " + (num1 >= num2) );
+		System.out.println("num1 <= num2 = " + (num1 <= num2) );
+
+	}
+}
---
+++ practical/shortdatatype.java
@@ -0,0 +1,10 @@
+package SACHIN;
+
+public class shortdatatype {
+	public static void main(String args[])
+	{
+	short myShort = 6000;
+	System.out.println("myShort: " + myShort);
+	}
+
+}
